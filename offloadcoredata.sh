22f07b9e4e982255716d376e5add40b36609c4d0
Epic Curious <109078515+epiccurious@users.noreply.github.com>
2023-01-08 17:07:12 -0500

    Add blocks/index/ directory to offloadcoredata.sh

--- offloadcoredata.sh
+++ offloadcoredata.sh
@@ -7,7 +7,7 @@ data_directory_source=$HOME/.bitcoin
 # The blocks target/destination directory, with no trailing slash
 # For our example, the drive is mounted at "/media/user1/DRIVE1"
 # and the target/destination directory is "timechain/"
-data_directory_target=/media/$USER/DRIVE/timechain
+data_directory_target=/media/$USER/DRIVE1/timechain
 
 # Set the source and target variables
 blocks_source=$data_directory_source/blocks
@@ -49,31 +49,29 @@ rm tomove
 # Copy the highest-numbered blk*.dat and rev*.dat
 # We will leave the original copy, rather than move.
 echo -n "Copying $highest_blk_dat and $highest_rev_dat... "
-cp $blocks_source/{$highest_blk_dat,highest_rev_dat} $blocks_target/
-echo "copied."
-
-# Copy the blocks index directory
-echo -n "Copying the blocks index... "
-cp $blocks_source/index/ $blocks_target/index/
+cp $blocks_source/$highest_{blk,rev}_dat $blocks_target/
 echo "copied."
 
 # TODO: Need to add a check if we're just removing and copying the same thing
-# Remove the old chainstate if one exists
-[ -d $chainstate_target/ ] && echo -n "Removing the old chainstate data" && rm -r $chainstate_target/ && echo "finished."
-# Remove the old configuration
-[ -f $configuration_target ] && echo -n "Removing the old bitcoin.conf file." && rm $configuration_target
+# Remove the old blocks index directory, if one exists
+[ -d $blocks_target/index/ ] && echo -n "Removing the old chainstate data" && rm -r $blocks_target/index/ && echo "finished."
+# Copy the blocks index directory
+echo -n "Copying the blocks index... "
+cp -r $blocks_source/index/ $blocks_target/index/
+echo "copied."
 
+# Remove the old chainstate directory, if one exists
+[ -d $chainstate_target/ ] && echo -n "Removing the old chainstate data" && rm -r $chainstate_target/ && echo "finished."
 # Copy the new chainstate
 echo -n "Copying the new chainstate ... "
 cp -r $chainstate_source/ $chainstate_target/
 echo "copied."
 
-# TODO: Need to add a check if we're just removing and copying the same thing
-
+# Remove the old configuration file, if one exists
+[ -f $configuration_target ] && echo -n "Removing the old configuration file." && rm $configuration_target
 # Copy the new configuration
+echo "Copying the new configuration file."
 cp $configuration_source $configuration_target
 
-echo "Finished copying the chainstate and configuration."
-
-echo "Finished transfer. Launching Bitcoin Core."
+echo "Finished the data transfer. Launching Bitcoin Core."
 $HOME/bitcoin/bin/bitcoin-qt &
