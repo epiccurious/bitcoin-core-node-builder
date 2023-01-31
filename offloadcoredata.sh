1ed377d2377b86c6c494441b769c293a810afce4
Epic Curious <109078515+epiccurious@users.noreply.github.com>
2023-01-31 11:42:28 -0500

    Simplify offloadcoredata.sh output and improve commenting

--- offloadcoredata.sh
+++ offloadcoredata.sh
@@ -9,9 +9,9 @@ data_directory_source="$HOME"/.bitcoin
 ## and the target/destination directory is "timechain/"
 data_directory_target=/media/$USER/DRIVE1/timechain
 
-# Name of the directory to extract into, without the trailing "/" (forward slash)
 bitcoin_core_extract_dir="$HOME/bitcoin"
 bitcoin_core_binary_dir="$bitcoin_core_extract_dir/bin"
+# Name of the directory to extract into, without the trailing / (forward slash)
 
 ## Set the source and target variables
 blocks_source=$data_directory_source/blocks
@@ -21,13 +21,14 @@ chainstate_target=$data_directory_target/chainstate
 
 ## Tell Bitcoin Core to stop and wait for the process to end
 clear
-echo -n "Closing Bitcoin Core..."
-## Get the process ID for bitcoin-qt
+echo -n "Closing Bitcoin Core."
+## Find bitcoin-qt's process ID
 qt_pid=$(pidof bitcoin-qt)
-## Send the stop command to bitcoin-cli
+## Send a stop command via RPC using bitcoin-cli
 "$bitcoin_core_binary_dir"/bitcoin-cli stop 1>/dev/null
-## Wait for the bitcoin-qt process to end
 while [[ $(pidof bitcoin-qt) == "$qt_pid" ]]; do echo -n "."; sleep 1; done
+## Wait for bitcoin-qt to stop
+## This step can take up to a couple minutes in certain cases with slow hardware.
 echo "closed."
 
 ## Search for all .dat files in the blocks directory and sort by the fourth character
@@ -42,38 +43,42 @@ echo {"${highest_blk_dat}","${highest_rev_dat}"} # for testing and development p
 grep -v "$highest_blk_dat" tomove > tmpfile && mv tmpfile tomove
 ## Remove the highest-numbered rev*.dat file from tomove
 grep -v "$highest_rev_dat" tomove > tmpfile && mv tmpfile tomove
-echo "Moving all .dat files except $highest_blk_dat and $highest_rev_dat."
 
+echo "Moving $(wc -l < tomove) block files."
 ## Iterate through each line of tomove
 while read -r file; do
-  echo -n "Moving and linking $file... "
   ## Move the blk*.dat and rev*.dat files
   rsync -ptgouq --partial --remove-source-files "$blocks_source"/"$file" "$blocks_target"/"$file"
   ## Create symbolic link from target to data dir
   ln -s "$blocks_target"/"$file" "$blocks_source"/"$file"
   ## Create a new line after rev files are copied
-  [[ $file == "rev"*".dat" ]] && echo "finished."
+  echo -n "."
 done <tomove
+echo "moved."
 ## Remove the list of files to move
 rm tomove
 
 ## Copy the highest-numbered blk*.dat and rev*.dat
 ## We will leave the original copy, rather than move.
-echo -n "Copying $highest_blk_dat and $highest_rev_dat... "
 rsync -ptgouq --partial "$blocks_source"/"$highest_blk_dat" "$blocks_target"/
 rsync -ptgouq --partial "$blocks_source"/"$highest_rev_dat" "$blocks_target"/
+echo -n "Copying 2 block files... "
 echo "copied."
 
-## Copy the blocks index
-echo -n "Copying the blocks index directory... "
 rsync -auq --partial --delete "$blocks_source"/index/ "$blocks_target"/index/
+## Copy the blocks/index/ directory
+## Delete old blocks index files left in the target directory
+echo -n "Copying the blocks index... "
 echo "copied."
 
 ## Copy the chainstate/ directory
 ## Delete old chainstate files left in the target directory
 rsync -auq --partial --delete "$chainstate_source"/ "$chainstate_target"/
+echo -n "Copying the unspent outputs... "
 echo "copied."
 
+echo "Finished transferring the blocks and UTXO data."
+
 ## Launch and disown bitcoin-qt
-echo "Finished the data transfer. Launching Bitcoin Core application."
-"$bitcoin_core_binary_dir"/bitcoin-qt & disown
+echo "Launching Bitcoin Core."
+"$bitcoin_core_binary_dir"/bitcoin-qt 2>/dev/null & disown
